0dfcc9d02c405f67342a147fc12111d547a946ce
Muhammed Ahmad <99442871+Muhammed-L-Ahmad@users.noreply.github.com>
2025-05-27 19:55:03 +0000

    Began working on job search api app

--- app.rb
+++ app.rb
@@ -1,9 +1,21 @@
 require "sinatra"
 require "sinatra/reloader"
 
+careerjet_api_key = ENV.fetch("SEARCH_API_KEY")
+
+# ask the user the title of the job they want to search for
+pp "What is the title of the job you'd like to search for?"
+
+  job_title = gets.chomp
+
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:root)
+end
+
+get("/search")
+  erb(:search)
+end
+
+get("/search/results")
+  erb(:search_results)
 end
