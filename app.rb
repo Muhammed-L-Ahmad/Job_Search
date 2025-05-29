21df5b05fc7a0c7354f7ca93bb4495d51c37ac39
Muhammed Ahmad <99442871+Muhammed-L-Ahmad@users.noreply.github.com>
2025-05-29 20:44:07 +0000

    Made more modifications trying to get it to work

--- app.rb
+++ app.rb
@@ -11,21 +11,28 @@ get("/results") do
   adzuna_app_id = ENV.fetch("ADZUNA_APP_ID")
   adzuna_app_key = ENV.fetch("ADZUNA_APP_KEY")
 
-  adzuna_app_url = "https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=#{adzuna_app_id}&app_key=#{adzuna_app_key}"
+  what = params.fetch "what"
+  where = params.fetch "where"
+
+  # TODO: Comment out everything and uncomment one piece at a time - not uncommenting the next piece until the previous piece works.
+
+  adzuna_app_url ="https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=#{adzuna_app_id}&app_key=#{adzuna_app_key}&results_per_page=1&what=#{what}&where=#{where}"
+  # adzuna_app_url = "https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=#{adzuna_app_id}&app_key=#{adzuna_app_key}&results_per_page=5&"+ what.gsub(" ") + where.gsub(" ", "%20") + "&key="
+
+  puts adzuna_app_url
   # TODO: send the request using HTTP class
   # Parse response using JSON class
   # Dig through the respnse
 
   raw_adzuna_response = HTTP.get(adzuna_app_url)
   parsed_adzuna_response = JSON.parse(raw_adzuna_response)
-  results_hash = parsed_adzuna_response.fetch("results")
+  parsed_adzuna_response.dig("Job", "Company", "Location")
+  # results_hash = parsed_adzuna_response.fetch("results")
   # first_result = results_hash.to_s
-  @what_hash = results_hash.fetch("Job").to_s
-  @who_hash = results_hash.fetch("Company").to_s
-  @where_hash = results_hash.fetch("Location").to_s
-# lat = location_hash.fetch("lat")
-# lng = location_hash.fetch("lng")
-  puts "Results for " + @what_hash.to_s + ", " + @where_hash.to_s + ": " + @results_hash
+  @what_hash = parsed_adzuna_response.fetch("Job")
+  @who_hash = parsed_adzuna_response.fetch("Company")
+  @where_hash = parsed_adzuna_response.fetch("Location")
+  puts "Results for " + @what_hash + ", " + @where_hash + ": " + @results_hash
   erb(:results)
 end
 
